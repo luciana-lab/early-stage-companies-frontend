8e87114265308b809d9390047f8be30844dcccca
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-01 14:13:18 -0500

    Company component dispatch and render

--- src/components/companies/CompanyList.js
+++ src/components/companies/CompanyList.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCompanies } from '../../actions/companiesActions'
 
 function CompanyList() {
+    const companies = useSelector(state => {
+        // console.log(state.companies)
+        return state.companies
+    })
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchCompanies())
+    }, [])
+
     return (
-        <div>Company</div>
+        <div>
+            {companies.map(company =>
+                <div>
+                    <h2>{company.name}</h2>
+                    <p>{company.industry}</p>
+                    <p>{company.description}</p>
+                    <p>{company.website}</p>
+                    <p>{company.image}</p>
+                    <p>{company.video}</p>
+                    <p>{company.need_category}</p>
+                    <p>{company.need}</p>
+                </div>
+            )}
+        </div>
     )
 }
 
