2c8b44f7a72b421522aace4880ba083af30a357e
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-03 15:22:35 -0500

    Contributions container fetch component

--- src/containers/ContributionsContainer.js
+++ src/containers/ContributionsContainer.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import ContributionForm from '../components/companies/contributions/ContributionForm';
 import Contributions from '../components/companies/contributions/Contributions';
 import { connect } from 'react-redux';
+import { fetchContributions } from './../actions/contributionsActions';
 
 class ContributionsContainer extends Component {
+    componentDidMount() {
+        this.props.fetchContributions()
+    }
+
     render() {
         return (
             <div>
                 <ContributionForm company={company} />
-                <Contributions companyId={company.id} />
+                <Contributions contributions={this.props.contributions} companyId={company.id} />
             </div>
         )
     }
@@ -20,4 +25,10 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(ContributionsContainer);
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchContributions: () => dispatch(fetchContributions())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContributionsContainer);
