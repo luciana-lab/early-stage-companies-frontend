8609a4535ffc79daa7ac96db073e4d5e3382c149
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-03 19:19:36 -0500

    Company deeply nested show page

--- src/components/companies/Companies.js
+++ src/components/companies/Companies.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Companies = ({ companies }) => {
+    // console.log(companies)
     return (
         <div>
             {companies.map(company => {
--- src/components/companies/Company.js
+++ src/components/companies/Company.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { removeCompany } from '../../actions/companiesActions';
 import { connect } from 'react-redux';
+import { removeCompany } from '../../actions/companiesActions';
 import ContributionForm from './contributions/ContributionForm';
-import ContributionsContainer from '../../containers/ContributionsContainer';
-// import Contributions from './contributions/Contributions';
-import { Link } from 'react-router-dom';
 
 class Company extends Component {
     constructor(props) {
         super(props)
-        this.company = props.companies.find(c => c.id === parseInt(this.props.routerProps.match.params.id))
+        // console.log(this.props.companies)
+        // console.log(this.props.routerProps.match.params.id)
+        this.company = this.props.companies.find(company => company.id === parseInt(this.props.routerProps.match.params.id))
+        console.log(this.company.contributions)
 
         this.state = {
-            contributionClicked: false
+            contributionBtn: false
         }
     }
 
@@ -25,16 +25,12 @@ class Company extends Component {
         this.props.routerProps.history.push("/companies")
     }
 
-    handleAddContribution = () => {
-        this.setState({ contributionClicked: true })
+    handleContributionClick = () => {
+        this.setState({ contributionBtn: true })
     }
 
-    // displayContributions = () => {
-    //     return <ContributionsContainer company={this.company} />
-    // }
-
-    displayContributionForm = () => {
-        if (this.state.contributionClicked === true) {
+    contributionForm = () => {
+        if (this.state.contributionBtn === true) {
             return <ContributionForm companyId={this.company.id} />
         }
     }
@@ -42,44 +38,34 @@ class Company extends Component {
     render() {
         return (
             <div key={this.company && this.company.id}>
-                <h2>{this.company && this.company.name}</h2>
+                <h3>{this.company && this.company.name}</h3>
+                <p>{this.company && this.company.video}</p>
                 <p>{this.company && this.company.industry}</p>
                 <p>{this.company && this.company.description}</p>
-                <p>{this.company && this.company.website}</p>
                 <p>{this.company && this.company.image}</p>
-                <p>{this.company && this.company.video}</p>
+                <p>{this.company && this.company.website}</p>
                 <p>{this.company && this.company.need_category}</p>
                 <p>{this.company && this.company.need}</p>
+                <br />
+
                 <button onClick={this.handleEditCompany}>Edit Company</button>
                 <button onClick={this.handleDeleteCompany}>Delete Company</button>
-                <br />
-                <p>{this.company && this.company.user.first_name}</p>
-                <p>{this.company && this.company.user.last_name}</p>
-                <p>{this.company && this.company.user.email}</p>
-                <p>{this.company && this.company.user.about}</p>
-                <p>{this.company && this.company.user.image}</p>
-                <br />
-                <h3>Contributions</h3>
-                {/* {this.company && this.company.contributions.map(contribution => {
+
+                {this.company && this.company.contributions.map(contribution => {
                     return (
                         <div key={contribution.id}>
-                            <Link to={`/users/${contribution.user_id}`}>
-                                <p>{contribution.user}</p>
-                                <p>{contribution.content}</p>
-                            </Link>
+                            <p>{contribution.user.first_name}</p>
+                            <p>{contribution.content}</p>
                         </div>
                     )
-                })} */}
-                <ContributionsContainer company={this.company && this.company} />
-                {/* {this.displayContributions()} */}
-                <button onClick={this.handleAddContribution}>Add a contribution</button>
-                {this.displayContributionForm()}
-                {/* <ContributionsContainer company={this.company} /> */}
+                })}
+                <br />
+
+                <button onClick={this.handleContributionClick}>Add Contribution</button>
+                {this.contributionForm()}
             </div>
         )
     }
-
-
 }
 
 export default connect(null, { removeCompany })(Company);
