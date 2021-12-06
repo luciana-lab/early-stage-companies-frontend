9a12fd9c877a35497b2ab36b16dd68273697a1c9
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-05 19:37:04 -0500

    Logo navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css';
+import '.././style/Navbar.css';
+import logo from '.././style/logo.png';
 
 function Navbar() {
     return (
         <nav>
             <ul>
+                <li><div id="logo"><img src={logo} className="img-responsive main-image" /></div></li>
                 <li><NavLink exact to="/">Home</NavLink></li>
                 <li><NavLink exact to="/companies">Companies</NavLink></li>
                 <li><NavLink exact to="/companies/new">Create Company</NavLink></li>
