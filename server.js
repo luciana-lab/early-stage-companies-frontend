eda72b6de852a1bb8e1f60199937e32340efdd06
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-10 18:15:26 -0500

    Heroku deploy try to resolve issues

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.js'));
 });
 
 app.listen(PORT);
